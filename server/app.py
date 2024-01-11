15afabd843d15172db95cd4433537bed0d55d767
calebmutisya <mutisyacalebmusau@gmail.com>
2024-01-11 15:48:27 +0300

    Post Succesful

--- server/app.py
+++ server/app.py
@@ -6,7 +6,7 @@ from flask_migrate import Migrate
 from models import db, Bakery, BakedGood
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
@@ -29,6 +29,40 @@ def bakery_by_id(id):
     bakery = Bakery.query.filter_by(id=id).first()
     bakery_serialized = bakery.to_dict()
     return make_response ( bakery_serialized, 200  )
+
+
+@app.route('/baked_goods', methods=['GET', 'POST'])
+def baked_goods():
+    if request.method == 'GET':
+        baked_goods=[]
+        for baked_good in BakedGood.query.all():
+            baked_good_dict= baked_good.to_dict()
+            baked_goods.append(baked_good_dict)
+
+        response= make_response(
+            baked_goods,200
+        )
+        return response
+    
+    elif request.method == 'POST':
+        new_baked_good= BakedGood(
+            name=request.form.get("name"),
+            price=request.form.get('price'),
+            bakery_id=request.form.get("bakery_id"),
+        )
+
+        db.session.add(new_baked_good)
+        db.session.commit()
+
+        baked_good_dict=new_baked_good.to_dict()
+
+        response= make_response(
+            baked_good_dict,201
+        )
+
+        return response
+
+
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
